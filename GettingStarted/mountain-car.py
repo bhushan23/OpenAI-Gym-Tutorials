2bc44807f21f14c3eb436e5f5abc0d63a4aa1edd
bhushan <bhushan.s.94@gmail.com>
2018-05-28 00:23:03 -0700

    Adding number of episodes

--- GettingStarted/mountain-car.py
+++ GettingStarted/mountain-car.py
@@ -1,16 +1,21 @@
 import gym
 env = gym.make('MountainCar-v0')
+num_episodes = 20
 # Uncomment following line to save video of our Agent interacting in this environment
 # This can be used for debugging and studying how our agent is performing
 # env = gym.wrappers.Monitor(env, './video/', force = True)
-t = 0
-while True:
-    t += 1
-    env.render()
-    print(observation)
-    action = env.action_space.sample()
-    observation, reward, done, info = env.step(action)
-    if done:
-        print("Episode finished after {} timesteps".format(t+1))
-        break
+
+# Number of times you want to train your agent to achieve the goal
+# Equivalent to number of epochs.
+for i in range(0, num_episodes)
+    t = 0
+    while True:
+        t += 1
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
 env.close()
